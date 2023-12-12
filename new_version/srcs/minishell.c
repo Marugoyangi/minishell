dc5d294881a3f2424f2d222e6f178ab6250596f0
Marugoyangi <125134255+Marugoyangi@users.noreply.github.com>
2023-12-13 00:30:41 +0900

    Add files via upload

--- new_version/srcs/minishell.c
+++ new_version/srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: jeongbpa <jeongbpa@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 09:18:37 by jeongbpa          #+#    #+#             */
-/*   Updated: 2023/12/13 00:23:46 by jeongbpa         ###   ########.fr       */
+/*   Updated: 2023/12/13 00:29:15 by jeongbpa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,12 @@ int	main(int argc, char **argv, char **envp)
 	
 	(void )argc;
 	terminal_init(&arg, &term, &original_term, envp);
-	if (!is_subshell)
+	if (!arg.is_subshell)
 		terminal_interactive(&term);
 	print_ascii();
 	while (1)
 	{
-		if (!is_subshell)
+		if (!arg.is_subshell)
 		{
 			arg.line.data = readline(get_ps1(&arg));
 			if (!arg.line.data)
@@ -37,7 +37,10 @@ int	main(int argc, char **argv, char **envp)
 			arg.line.data = modified_strtrim(arg.line.data, " \t\n");
 		}
 		else
-			arg.line.data = ft_strdup(argv[1]);
+		{
+			arg.line.data = ft_strdup(argv[0]);
+			arg.line.data = modified_strtrim(arg.line.data, "()");
+		}
 		tokenize(&arg.line, &arg);
 		lexicize(&arg);
 		expand_vars(&arg);
@@ -53,7 +56,7 @@ int	main(int argc, char **argv, char **envp)
 		free_ast(arg.ast_head);
 		arg.ast_head = NULL;
 		terminal_interactive(&term);
-		if (is_subshell)
+		if (arg.is_subshell)
 			exit (0);
 	}
 	free_arg(&arg);
